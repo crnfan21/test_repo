24999f9b4dcb3d895ffcf4914ad6e49d77099f79
ojw <crnfan21@naver.com>
2025-01-09 10:28:00 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,5 +18,6 @@ public class Main {
         System.out.println("commit1");
         System.out.println("commit2");
         System.out.println("commit3");
+        System.out.println("pr commit1");
     }
 }
