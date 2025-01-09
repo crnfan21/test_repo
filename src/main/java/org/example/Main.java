8ce50869a85fed137d0613cda6dc0bb73a0613fb
ojw <crnfan21@naver.com>
2025-01-09 10:28:24 +0900

    pull request

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,5 +20,6 @@ public class Main {
         System.out.println("commit3");
         System.out.println("pr commit1");
         System.out.println("pr commit2");
+        System.out.println("pull request7");
     }
 }
